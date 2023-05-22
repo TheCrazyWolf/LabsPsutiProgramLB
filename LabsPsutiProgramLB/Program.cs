46c5644384863a889fc0ebe9e58146184ea1e5e7
526-0\kulagin <arvrsgk@gmail.com>
2023-05-22 18:51:44 +0400

    Добавьте файлы проекта.

---
+++ LabsPsutiProgramLB/Program.cs
@@ -0,0 +1,35 @@
+﻿using ScottPlot;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double h = 0.05;
+        double x = -10;
+        double endX = 10;
+
+        double[] xData = new double[(int)((endX - x) / h) + 1];
+        double[] yData = new double[xData.Length];
+
+        for (int i = 0; i < xData.Length; i++)
+        {
+            double y = 1.0 / (x + 1) + 2;
+            xData[i] = x;
+            yData[i] = y;
+            x += h;
+        }
+
+        // Создание графического окна
+        var plt = new Plot(600, 400);
+        plt.PlotScatter(xData, yData);
+        plt.Title("График функции y(x)");
+        plt.XLabel("x");
+        plt.YLabel("y");
+
+        // Отображение графика
+        plt.SaveFig("graph.png"); // Сохранение графика в файл
+        plt.Render(); // Отображение графика в окне
+
+        Console.ReadKey();
+    }
+}
---
+++ LabsPsutiProgrammLB2/Program.cs
@@ -0,0 +1,56 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int screenWidth = 80;
+        int screenHeight = 20;
+        int ballX = 0;
+        int ballY = 0;
+        int directionX = 1;
+        int directionY = 1;
+
+        while (true)
+        {
+            Console.Clear();
+
+            ballX += directionX;
+            ballY += directionY;
+
+            if (ballX >= screenWidth)
+            {
+                ballX = screenWidth - 1;
+                directionX = 0;
+                directionY = 1;
+            }
+            else if (ballX < 0)
+            {
+                ballX = 0;
+                directionX = 0;
+                directionY = -1;
+            }
+
+            if (ballY >= screenHeight)
+            {
+                ballY = screenHeight - 1;
+                directionX = -1;
+                directionY = 0;
+            }
+            else if (ballY < 0)
+            {
+                ballY = 0;
+                directionX = 1;
+                directionY = 0;
+            }
+
+            DrawBall(ballX, ballY);
+
+            Thread.Sleep(100); // Задержка для плавности движени
+        }
+
+        static void DrawBall(int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write("O");
+        }
+    }
+}
